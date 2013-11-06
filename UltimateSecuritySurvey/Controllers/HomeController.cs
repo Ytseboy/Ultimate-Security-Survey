b0c21aceceeeacb403741f2b152dddb30a435a17
Slikd <topiasikainen@hotmail.com>
2013-11-06 15:24:07 +0200

    This is dangerous

---
+++ UltimateSecuritySurvey/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UltimateSecuritySurvey.Models;
+namespace UltimateSecuritySurvey.Controllers
+{
+    public class HomeController : Controller
+    {
+   
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            FrontPageModel question = new FrontPageModel();
+            return View(question);
+        }
+
+    }
+}
---
+++ UltimateSecuritySurvey/Models/FrontPageModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UltimateSecuritySurvey.Models
+{
+    public class FrontPageModel
+    {
+        SecuritySurveyEntities db = new SecuritySurveyEntities();
+
+        public FrontPageModel() 
+        {
+            this.myQuestion = db.Questions.Find("q1");
+        }
+
+        public Question myQuestion { get; set; }
+    }
+}
