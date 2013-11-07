aad644f1077b48520dbfba6f35df5e03af8f7107
Alex Shkunov <toman22@yandex.ru>
2013-11-07 22:54:29 +0200

    Add AboutController and view + Copy Topi's about text there

---
+++ UltimateSecuritySurvey/Controllers/AboutController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UltimateSecuritySurvey.Controllers
+{
+    /// <summary>
+    /// This controller doesn't perform any task
+    /// I simply returns a View where user can read
+    /// About application
+    /// </summary>
+    public class AboutController : Controller
+    {
+        /// <summary>
+        /// About application
+        /// </summary>
+        /// <returns>Index view</returns>
+        //
+        // GET: /About/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
