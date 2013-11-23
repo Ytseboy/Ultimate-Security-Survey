ccd315e85c0732f6a4b6e2a752992f48f9db61da
Alex Shkunov <toman22@yandex.ru>
2013-11-24 00:52:51 +0200

    Login functionality + All the pages private from now

---
+++ UltimateSecuritySurvey/Controllers/AccountController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using UltimateSecuritySurvey.Models;
+
+namespace UltimateSecuritySurvey.Controllers
+{   
+    /// <summary>
+    /// To control Log In / Log out
+    /// </summary>
+    public class AccountController : Controller
+    {
+        private SecuritySurveyEntities db = new SecuritySurveyEntities();
+
+        /// <summary>
+        /// Redirects user to LogIn form
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Handles attempt to log in
+        /// </summary>
+        /// <param name="loginData">User name + password</param>
+        /// <returns>Dashboar View</returns>
+        [HttpPost]
+        public ActionResult Login(string name, string password)
+        {
+            UserAccount userAccount = db.UserAccounts.FirstOrDefault(x => (x.userName == name || x.email == name) 
+                                                            && x.password == password);
+            if (userAccount != null)
+            {
+                FormsAuthentication.SetAuthCookie(userAccount.userName, true);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "User Name OR password is incorrect");
+            }
+            return View(new { name = name, password = password});
+        }
+
+        /// <summary>
+        /// To logOut, redirects user to Login form
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Account");
+        }
+    }
+}
