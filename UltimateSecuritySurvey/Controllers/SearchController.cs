ad7e911bb014ad14317d44703f00b002f4c9d3f4
Ytseboy <danfreitaschn+programming@gmail.com>
2013-12-13 21:47:34 +0200

    Basic search functionality view and controller

---
+++ UltimateSecuritySurvey/Controllers/SearchController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UltimateSecuritySurvey.Models;
+
+namespace UltimateSecuritySurvey.Controllers
+{
+    public class SearchController : Controller
+    {
+        //
+        // GET: /Search/
+        private readonly SecuritySurveyEntities db = new SecuritySurveyEntities();
+        public ActionResult Index(string searchString)
+        {
+            var questions = from question in db.Questions
+                            select question;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                questions = questions.Where(s => s.questionTextMain.Contains(searchString));
+            }
+
+            return View(questions);
+        }
+
+    }
+}
