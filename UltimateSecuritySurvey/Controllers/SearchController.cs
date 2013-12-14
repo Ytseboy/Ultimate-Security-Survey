9aae434a1630d87a7a89336d7e71509dd0eb0c1d
Alex Shkunov <toman22@yandex.ru>
2013-12-14 22:16:02 +0200

    Update User guide + Make mathing string part to be bold in Search view

--- UltimateSecuritySurvey/Controllers/SearchController.cs
+++ UltimateSecuritySurvey/Controllers/SearchController.cs
@@ -7,21 +7,21 @@ using UltimateSecuritySurvey.Models;
 
 namespace UltimateSecuritySurvey.Controllers
 {
+    [Authorize]
     public class SearchController : Controller
     {
-        //
-        // GET: /Search/
         private readonly SecuritySurveyEntities db = new SecuritySurveyEntities();
         public ActionResult Index(string searchString)
         {
             var questions = from question in db.Questions
                             select question;
 
-            if (!String.IsNullOrEmpty(searchString))
+            if (!String.IsNullOrEmpty(searchString) && searchString.Length > 1)
             {
                 questions = questions.Where(s => s.questionTextMain.Contains(searchString));
+                ViewBag.query = searchString;
             }
-
+            
             return View(questions);
         }
 
